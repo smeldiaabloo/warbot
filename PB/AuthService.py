cdc0eb488d43374e0062285001475b292b1da0c1
PRANKBOTS <33993971+Aprank@users.noreply.github.com>
2018-06-06 22:42:55 +0700

    Update AuthService.py

--- PB/AuthService.py
+++ PB/AuthService.py
@@ -8,7 +8,7 @@
 
 from thrift.Thrift import TType, TMessageType, TFrozenDict, TException, TApplicationException
 from thrift.protocol.TProtocol import TProtocolException
-from thrift.TRecursive import fix_spec
+#from thrift.TRecursive import fix_spec
 
 import sys
 import logging
